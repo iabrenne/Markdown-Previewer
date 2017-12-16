ceaf9a9966333c8bbfa9cd03f6cb43be337a56c6
Irina Brennen <irina.brennen@twc.com>
2017-12-15 20:29:45 -0600

    Added pikachu svg and a few lines of sample markup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import logo from './my_image.svg';
 import './App.css';
 var marked = require('marked');
 
 
+
 class App extends Component {
 
+  
+
   constructor(props){
+
     super(props);
+
+    let sampleMarkdown = "# This is a Header (H1 size) for my Markdown Previewer\n";
+    sampleMarkdown += "## This is a subheader (H2 size)\n";
+  
     this.state = {
-      editorText: '__I am groot__'
+      editorText: sampleMarkdown
     };
 
   }
@@ -24,14 +33,14 @@ class App extends Component {
       <div id="main-div">
         <h1>Markdown Editor</h1>
         
-        <textarea id="editor" onChange={this.handleChange}>
-          {this.state.editorText}
+        <textarea id="editor" onInput={this.handleChange} onChange={this.handleChange}>
+                  {this.state.editorText}
         </textarea>
         
         <h1>Markdown Previewer</h1>
         <div id="preview" dangerouslySetInnerHTML={{ __html:marked(this.state.editorText) }}>
         </div>
-
+         <img src={logo} alt="logo" />
       </div>
     );
   }
